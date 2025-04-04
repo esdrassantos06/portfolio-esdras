cc47ae399611c7fb15142f7fb8782601a3a55628
esdrassantos06 <esdrasirion1@gmail.com>
2025-04-04 18:58:13 +0100

    refactor: Update Footer component for improved structure and animation
    - Replaced the outer ScrollAnimation component with a div for better structure and maintainability.
    - Adjusted initialY values in ScrollAnimation components to enhance the animation effect.
    - Cleaned up the layout by removing unnecessary nested ScrollAnimation components, improving readability and performance.
    - Ensured consistent styling and responsiveness across the footer sections.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,10 @@ import ScrollAnimation from "./ScrollAnimation";
 
 export default function Footer() {
   return (
-    <ScrollAnimation delayIndex={0} initialY={20} className="lg:w-[65%] w-[80%] relative mb-8 mx-auto bottom-4 rounded-2xl flex flex-col items-center justify-between border-gray-200/20 border-1 h-fit lg:h-120">
-      <footer
-        id="contact"
-        className="w-full h-fit"
-      >
+    <div className="lg:w-[65%] w-[80%] relative mb-8 mx-auto bottom-4 rounded-2xl flex flex-col items-center justify-between border-gray-200/20 border-1 h-fit lg:h-120 overflow-hidden">
+      <footer id="contact" className="w-full h-fit">
         <div className="first-part-footer p-8 pt-10 flex flex-col w-full items-start justify-start gap-8">
-          <ScrollAnimation delayIndex={1}>
+          <ScrollAnimation delayIndex={1} initialY={10}>
             <h1 className="text-4xl md:text-5xl max-w-full w-fit lg:w-160 font-bold">
               Like what you see? Reach out{" "}
               <a
@@ -24,7 +21,7 @@ export default function Footer() {
               to collaborate!
             </h1>
           </ScrollAnimation>
-          <ScrollAnimation delayIndex={2}>
+          <ScrollAnimation delayIndex={2} initialY={10}>
             <ShinyButton
               showArrow={false}
               text="Schedule call"
@@ -36,62 +33,52 @@ export default function Footer() {
         <div className="border-t w-full border-gray-200/20"></div>
 
         <div className="second-part-footer p-8 flex md:flex-row flex-col gap-10 justify-between w-full">
-          <div className="name-and-copy flex flex-col gap-2">
-            <ScrollAnimation delayIndex={3} initialY={20}>
+          <ScrollAnimation delayIndex={3} initialY={10}>
+            <div className="name-and-copy flex flex-col gap-2">
               <h1 className="text-xl font-bold">Esdras Santos</h1>
               <span className="text-principal/70 text-base">
                 © {new Date().getFullYear()} | All rights reserved.
               </span>
-            </ScrollAnimation>
-          </div>
+            </div>
+          </ScrollAnimation>
 
           <nav className="footer-navbar flex md:flex-nowrap flex-wrap justify-between gap-6 md:gap-2 md:w-1/3">
-            <div className="navigate">
-              <ScrollAnimation delayIndex={4} initialY={20}>
+            <ScrollAnimation delayIndex={4} initialY={10}>
+              <div className="navigate">
                 <h1 className="text-principal text-xl font-semibold">
                   Navigate
                 </h1>
-              </ScrollAnimation>
-              <ul className="flex flex-col mt-1 gap-2">
-                <ScrollAnimation delayIndex={4} initialY={20}>
+                <ul className="flex flex-col mt-1 gap-2">
                   <li className="hover:text-principal font-light text-principal/70">
                     <Link aria-label="Home" href={"/#home"}>
                       Home
                     </Link>
                   </li>
-                </ScrollAnimation>
-                <ScrollAnimation delayIndex={5} initialY={20}>
                   <li className="hover:text-principal font-light text-principal/70">
                     <Link aria-label="About Me" href={"/#about"}>
                       About
                     </Link>
                   </li>
-                </ScrollAnimation>
-                <ScrollAnimation delayIndex={6} initialY={20}>
                   <li className="hover:text-principal font-light text-principal/70">
                     <Link aria-label="My Work" href={"/#work"}>
                       Work
                     </Link>
                   </li>
-                </ScrollAnimation>
-                <ScrollAnimation delayIndex={7} initialY={20}>
                   <li className="hover:text-principal font-light text-principal/70">
                     <Link aria-label="Contact" href={"/#contact"}>
                       Contact
                     </Link>
                   </li>
-                </ScrollAnimation>
-              </ul>
-            </div>
+                </ul>
+              </div>
+            </ScrollAnimation>
 
-            <div className="projects">
-              <ScrollAnimation delayIndex={5} initialY={20}>
+            <ScrollAnimation delayIndex={5} initialY={10}>
+              <div className="projects">
                 <h1 className="text-principal text-xl font-semibold">
                   Projects
                 </h1>
-              </ScrollAnimation>
-              <ul className="flex flex-col mt-1 gap-2">
-                <ScrollAnimation delayIndex={5} initialY={20}>
+                <ul className="flex flex-col mt-1 gap-2">
                   <li className="hover:text-principal font-light text-principal/70">
                     <Link
                       aria-label="Project Nova Horizonte"
@@ -100,32 +87,26 @@ export default function Footer() {
                       Nova Horizonte
                     </Link>
                   </li>
-                </ScrollAnimation>
-                <ScrollAnimation delayIndex={6} initialY={20}>
                   <li className="hover:text-principal font-light text-principal/70">
                     <Link aria-label="Tarevity" href={"/projects/tarevity"}>
                       Tarevity
                     </Link>
                   </li>
-                </ScrollAnimation>
-                <ScrollAnimation delayIndex={7} initialY={20}>
                   <li className="hover:text-principal font-light text-principal/70">
                     <Link aria-label="Coming Soon" href={"/"}>
                       Coming Soon...
                     </Link>
                   </li>
-                </ScrollAnimation>
-              </ul>
-            </div>
+                </ul>
+              </div>
+            </ScrollAnimation>
 
-            <div className="socials">
-              <ScrollAnimation delayIndex={6} initialY={20}>
+            <ScrollAnimation delayIndex={6} initialY={10}>
+              <div className="socials">
                 <h1 className="text-principal text-xl font-semibold">
                   Socials
                 </h1>
-              </ScrollAnimation>
-              <ul className="flex flex-col mt-1 gap-2">
-                <ScrollAnimation delayIndex={6} initialY={20}>
+                <ul className="flex flex-col mt-1 gap-2">
                   <li className="hover:text-principal font-light text-principal/70">
                     <a
                       rel="noopener noreferrer"
@@ -136,8 +117,6 @@ export default function Footer() {
                       Linkedin
                     </a>
                   </li>
-                </ScrollAnimation>
-                <ScrollAnimation delayIndex={7} initialY={20}>
                   <li className="hover:text-principal font-light text-principal/70">
                     <a
                       href="https://github.com/esdrassantos06"
@@ -148,12 +127,12 @@ export default function Footer() {
                       Github
                     </a>
                   </li>
-                </ScrollAnimation>
-              </ul>
-            </div>
+                </ul>
+              </div>
+            </ScrollAnimation>
           </nav>
         </div>
       </footer>
-    </ScrollAnimation>
+    </div>
   );
 }
