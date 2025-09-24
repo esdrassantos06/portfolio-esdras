d192222fb665bcd57d6aa7aa5c629d09e52cef1d
Esdras Santos <esdrasirion1@gmail.com>
2025-09-24 13:16:14 +0100

    fix: Adjust class name order in Work component for better layout consistency
    - Modified the class name order in the title-and-button div to enhance layout responsiveness and maintain visual consistency across different screen sizes.

--- src/components/sections/Work.tsx
+++ src/components/sections/Work.tsx
@@ -11,7 +11,7 @@ export default async function Work() {
       className="mx-auto my-20 flex min-h-screen w-3/4 items-center justify-center"
     >
       <div className="flex w-full flex-col items-center justify-center">
-        <div className="title-and-button mb-20 flex w-full flex-col items-center justify-between gap-4 self-start px-2 lg:flex-row md:gap-6">
+        <div className="title-and-button mb-20 flex w-full flex-col items-center justify-between gap-4 self-start px-2 md:gap-6 lg:flex-row">
           <ScrollAnimation delayIndex={0}>
             <h1 className="mt-2 text-center text-5xl font-bold sm:text-6xl">
               {t("title")}
